5984aa3a74b40fb5279a5fbfa1587639a8cfae8a
John Coonrod <john.coonrod@thp.org>
2021-08-18 17:44:10 -0400

    trying deply

---
+++ go
@@ -0,0 +1,2 @@
+export PATH="/home/johncoonrod/.deno/bin:/usr/local/bin:/usr/bin:/bin"
+deployctl run hello.ts
--- hello.ts
+++ hello.ts
@@ -1,1 +1,3 @@
-hello
+// deploy version of hello world
+const r=new Response("<h1>Greetings Earthlings!</h1>", {status: 200, headers:{"Content-type":"text/html" } })
+addEventListener('fetch', e=>e.respondWith(r));
