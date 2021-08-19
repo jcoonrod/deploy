54d0621e3366a2e0f36712929b8d429890785ceb
John Coonrod <john.coonrod@thp.org>
2021-08-18 20:44:00 -0400

    add full page body

--- hello.ts
+++ hello.ts
@@ -1,3 +1,15 @@
 // deploy version of hello world
-const r=new Response("<h1>Greetings Earthlings!</h1>", {status: 200, headers:{"Content-type":"text/html" } });
+const body=`<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <title>Deploy</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="description" content="Learning Deno Deploy.">
+    </head>
+    <body>
+        <a href=/><h1>Greetings Earthlings</h1></a>
+    </body>
+</html>`;
+const r=new Response(body, {status: 200, headers:{"Content-type":"text/html" } });
 addEventListener('fetch', e=>e.respondWith(r));
