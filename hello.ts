db6de8b923292c088b48b76881c5063a70222bb7
John Coonrod <john.coonrod@thp.org>
2021-08-18 20:39:31 -0400

    debug semicolon

--- hello.ts
+++ hello.ts
@@ -1,3 +1,3 @@
 // deploy version of hello world
-const r=new Response("<h1>Greetings Earthlings!</h1>", {status: 200, headers:{"Content-type":"text/html" } })
+const r=new Response("<h1>Greetings Earthlings!</h1>", {status: 200, headers:{"Content-type":"text/html" } });
 addEventListener('fetch', e=>e.respondWith(r));
